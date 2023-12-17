0c83c57dc7482294dd7df7f8157ad89d9612e4c7
Volha Milk <olkamiroyka@gmail.com>
2023-12-17 17:35:40 +0300

    Commit homework

---
+++ src/homeWorks/HW1_2.java
@@ -0,0 +1,11 @@
+package homeWorks;
+
+import java.util.Scanner;
+
+public class HW1_2 {
+    public static void main(String[] args) {
+        Scanner thing = new Scanner(System.in);
+        String name = thing.nextLine();
+        System.out.println(name);
+    }
+}
---
+++ src/homeWorks/hw1_1.java
@@ -0,0 +1,7 @@
+package homeWorks;
+
+public class hw1_1 {
+    public static void main(String [] args){
+        System.out.println("Hello world!");
+    }
+}
