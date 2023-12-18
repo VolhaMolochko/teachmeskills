bcdedad02ecf83228e14911ec8f62fad1525da3b
Volha Milk <olkamiroyka@gmail.com>
2023-12-18 21:51:40 +0300

    пример 3

---
+++ src/lessons/LessonTwoPracticeThree.java
@@ -0,0 +1,12 @@
+package lessons;
+
+public class LessonTwoPracticeThree {
+    public static void main(String[] args) {
+        int fakt=5;
+        int rez=1;
+        for (int a=1; a<= fakt; a++)
+        {  rez=a * rez;
+        }
+        System.out.println(rez);
+    }
+}
