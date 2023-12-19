dfc33f48ad1114e46bd9385a06588421f2a58212
Volha Milk <olkamiroyka@gmail.com>
2023-12-20 00:20:02 +0300

    Задача 5

---
+++ src/lesson2homework/Task5.java
@@ -0,0 +1,18 @@
+package lesson2homework;
+//По введенному номеру определить цвет радуги
+public class Task5 {
+    public static void main(String [] args) {
+int num;
+num=2;
+        switch (num) {
+            case 1 -> System.out.println("Red");
+            case 2 -> System.out.println("Orange");
+            case 3 -> System.out.println("Yellow");
+            case 4 -> System.out.println("Green");
+            case 5 -> System.out.println("Blue");
+            case 6 -> System.out.println("Indigo");
+            case 7 -> System.out.println("Violet");
+            default -> System.out.println("Нет такого цвета");
+
+    }
+}}
