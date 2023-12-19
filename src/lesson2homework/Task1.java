77221f2443ea9f1c1db956a1180524654cc80cb9
Volha Milk <olkamiroyka@gmail.com>
2023-12-19 22:13:24 +0300

    Задача 1 ДЗ 2

---
+++ src/lesson2homework/Task1.java
@@ -0,0 +1,29 @@
+package lesson2homework;
+
+// написать программу для вывода названия поры года по месяцу
+//использовать switch case
+
+public class Task1 {
+    public static void main(String[] args) {
+        int num;
+        num = 12;
+        switch (num) {
+            case 1 -> System.out.println("Winter");
+            case 2 -> System.out.println("Winter");
+            case 3 -> System.out.println("Spring");
+            case 4 -> System.out.println("Spring");
+            case 5 -> System.out.println("Spring");
+            case 6 -> System.out.println("Summer");
+            case 7 -> System.out.println("Summer");
+            case 8 -> System.out.println("Summer");
+            case 9 -> System.out.println("Autumn");
+            case 10 -> System.out.println("Autumn");
+            case 11 -> System.out.println("Autumn");
+            case 12 -> System.out.println("Winter");
+
+
+        }
+
+
+    }
+}
