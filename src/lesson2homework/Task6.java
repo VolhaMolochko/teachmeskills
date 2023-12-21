c4e509e1ea9e06d7a067bba91c1e94f77dde1a17
Volha Milk <olkamiroyka@gmail.com>
2023-12-21 14:18:22 +0300

    Задача 6

---
+++ src/lesson2homework/Task6.java
@@ -0,0 +1,16 @@
+package lesson2homework;
+
+// При помощи цикла for высести на экран нечетные числа от 1 до 99.
+// При решении используйте операцию инкремента (++)
+public class Task6 {
+    public static void main(String[] args) {
+        int nech;
+        nech = 1;
+        for (int i = 0; i <= 99; i++) {
+            if (i % 2 != 0) {
+                nech = i;
+                System.out.println(nech);
+            }
+        }
+    }
+}
