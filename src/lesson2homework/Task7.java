caa07b3b9e465729fca8ec322b2d4933b1249d6b
Volha Milk <olkamiroyka@gmail.com>
2023-12-21 14:30:29 +0300

    Задача 7

---
+++ src/lesson2homework/Task7.java
@@ -0,0 +1,13 @@
+package lesson2homework;
+
+//Необходимо вывести на экран числа от 5 до 1.
+//При решении используйте операцию декремента(--)
+public class Task7 {
+    public static void main(String[] args) {
+        int chis;
+        for (int i = 5; i >= 1; i--) {
+            chis = i;
+            System.out.println(chis);
+        }
+    }
+}
