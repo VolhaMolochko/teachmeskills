35242e60cf7e6138e347ae69ec9e9cb254d90e1d
Volha Milk <olkamiroyka@gmail.com>
2023-12-23 21:40:45 +0300

    Задача 2 (первое решение //)

--- src/lesson2homework/Task2.java
+++ src/lesson2homework/Task2.java
@@ -1,22 +1,22 @@
 package lesson2homework;
 //написать программу для вывода названия поры года по номеру месяца, используя if else
-//public class Task2 {
-//    public static void main (String [] args) {
-//        int mon;
-//        mon = 4;
-//        boolean sp=((mon>=3)&&(mon<=5));
-//        boolean sam=((mon>=6)&&(mon<=8));
-//        boolean aut=((mon>=9)&&(mon<=11));
-//        boolean win=((mon==12)||(mon==1)||(mon==2));
-//        if(win){
-//            System.out.println("Winter");
-//        }else if(sp) {
-//            System.out.println("Spring");
-//        }else if(sam) {
-//            System.out.println("Summer");
-//        }else System.out.println("Autumn");
-//        }
-//    }
+public class Task2 {
+    public static void main (String [] args) {
+        int mon;
+        mon = 4;
+        boolean sp=((mon>=3)&&(mon<=5));
+        boolean sam=((mon>=6)&&(mon<=8));
+        boolean aut=((mon>=9)&&(mon<=11));
+        boolean win=((mon==12)||(mon==1)||(mon==2));
+        if(win){
+            System.out.println("Winter");
+        }else if(sp) {
+            System.out.println("Spring");
+        }else if(sam) {
+            System.out.println("Summer");
+        }else System.out.println("Autumn");
+        }
+    }
 
 //public class Task2 {
 //    public static void main (String [] args) {
