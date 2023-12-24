90a50e1131b3045d194fbe9ce89182e3f8e617de
Volha Milk <olkamiroyka@gmail.com>
2023-12-24 22:48:09 +0300

    Домашняяя работа 3. Задаа 1

---
+++ src/lesson3homework/Task1.java
@@ -0,0 +1,37 @@
+package lesson3homework;
+
+import java.util.Scanner;
+
+//Создайте массив целых чисел. Напишите программу, которая выводит
+//сообщение о том, входит ли заданное число в массив или нет.
+//Пусть число для поиска задачется из консоли
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); //задаем переменную сканер, значение которой будем выводить с клавиатуры
+        System.out.println("Введите число");
+        int a;
+        a = 0;
+        int chislo = scanner.nextInt(); //это уже само значение которое потом можно будет использовать
+        int[] massiv = {2, 12, 46, 10, 35, 7};
+        for (int i = 0; i < massiv.length; i++) {
+            if (massiv[i] == chislo) {
+                a++;
+            }
+        }
+        if (a == 0) {
+            System.out.println("Такое число отсутствует в массиве");
+        } else {
+            System.out.println("Такое число есть в массиве");
+        }
+
+    }
+
+}
+
+
+
+
+
+
+
+
