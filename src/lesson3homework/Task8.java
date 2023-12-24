a627927cc628be0a05cd57e1f85aacd81f2b6927
Volha Milk <olkamiroyka@gmail.com>
2023-12-25 01:17:53 +0300

    Домашняяя работа 3. Задача 4 заменила стандартный for на for (int i : array)

---
+++ src/lesson3homework/Task8.java
@@ -0,0 +1,26 @@
+package lesson3homework;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+//Реализуйте алгоритм сортировки пузырьком
+public class Task8 {
+    public static void main(String[] args) {
+        int[] zorka = {12, 7, 4, 42, 13, 8, 2};
+        int buf;
+        boolean otsortirovan = false;
+        while (!otsortirovan) {
+            otsortirovan = true;
+            for (int i = 0; i < zorka.length - 1; i++) {
+                if (zorka[i] > zorka[i + 1]) {
+                    otsortirovan = false;
+                    buf = zorka[i];
+                    zorka[i] = zorka[i + 1];
+                    zorka[i + 1] = buf;
+                    System.out.println(Arrays.toString(zorka));
+                }
+            }
+        }
+    }
+}
+
